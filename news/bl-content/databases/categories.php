d44996cfed6f9efc48adb690b9ec9fc70c7f51f8
koleon03 <admin@koleon03.me>
2021-04-03 22:02:49 +0200

    Initial commit

---
+++ news/bl-content/databases/categories.php
@@ -0,0 +1,2 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{"general":{"name":"General","description":"","template":"","list":[]},"music":{"name":"Music","description":"","template":"","list":[]},"videos":{"name":"Videos","description":"","template":"","list":[]}}
---
+++ news/bl-content/databases/plugins/about/db.php
@@ -0,0 +1,2 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{"position":1,"label":"About","text":"This is the news homepage for the Koleon Cult."}
